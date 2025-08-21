eaec9ea9dd0711a55f150b905c05da474208dd7a
Rohit Ghate <99409706+rohitghate29@users.noreply.github.com>
2025-08-21 14:03:30 +0530

    Update App.jsx
    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import CoursePage from "./components/Course/CoursePage";
 import Signup from "./components/signup/Signup";
 import Contact from "./components/contact/Contact";
+import Products from "./Products";
 import toast, { Toaster } from "react-hot-toast";
 import { useAuth } from "./components/context/AuthProvider";
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/course" element={ authUser ? <CoursePage /> : <Navigate to="/signup" />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
         <Toaster />
       </div>
